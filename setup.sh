61ce69b23208de516b92dc58cda5b1d80cf187a4
Takashi Masuyama <mamewotoko@gmail.com>
2021-08-29 07:15:46 +0900

    add ci file

--- setup.sh
+++ setup.sh
@@ -3,31 +3,32 @@
 
 if [ "$UNAME" = Darwin ]; then
     brew update
-    brew install --build-from-source gtk+3
+    brew install gtk4
     # make gcc pkg-config
-
-elif [ -f /etc/lsb-release ]; then
-    sudo apt-get update
-    sudo apt-get install -y libgtk-3-dev pkg-config \
-         xvfb make gcc
-
-elif [ -f /etc/redhat-release ] && grep "release 7" /etc/redhat-release; then
-    sudo yum install -y gcc gobject-introspection-devel \
-         pkg-config gtk3 make
-
-elif [[ "$UNAME" == "MINGW64_NT"* ]]; then
-    # msys2 on PC
-    pacman -Syu --noconfirm
-    pacman -Sy --noconfirm mingw-w64-x86_64-gtk3 \
-           mingw-w64-x86_64-make \
-           mingw-w64-x86_64-gcc \
-           mingw-w64-x86_64-pkg-config
-
-elif [[ "$UNAME" == "MSYS_NT"* ]]; then
-    # travis
-    $msys2 pacman -Syu --noconfirm
-    $msys2 pacman -Sy --noconfirm mingw-w64-x86_64-gtk3 \
-           mingw-w64-x86_64-make \
-           mingw-w64-x86_64-gcc \
-           mingw-w64-x86_64-pkg-config
 fi
+
+# elif [ -f /etc/lsb-release ]; then
+#     sudo apt-get update
+#     sudo apt-get install -y libgtk-3-dev pkg-config \
+#          xvfb make gcc
+
+# elif [ -f /etc/redhat-release ] && grep "release 7" /etc/redhat-release; then
+#     sudo yum install -y gcc gobject-introspection-devel \
+#          pkg-config gtk3 make
+
+# elif [[ "$UNAME" == "MINGW64_NT"* ]]; then
+#     # msys2 on PC
+#     pacman -Syu --noconfirm
+#     pacman -Sy --noconfirm mingw-w64-x86_64-gtk3 \
+#            mingw-w64-x86_64-make \
+#            mingw-w64-x86_64-gcc \
+#            mingw-w64-x86_64-pkg-config
+
+# elif [[ "$UNAME" == "MSYS_NT"* ]]; then
+#     # travis
+#     $msys2 pacman -Syu --noconfirm
+#     $msys2 pacman -Sy --noconfirm mingw-w64-x86_64-gtk3 \
+#            mingw-w64-x86_64-make \
+#            mingw-w64-x86_64-gcc \
+#            mingw-w64-x86_64-pkg-config
+# fi
