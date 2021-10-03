d1bf55a604c04142f5b2a14347eb493eca2a7945
Takashi Masuyama <mamewotoko@gmail.com>
2021-10-03 12:58:01 +0900

    add ci on ubuntu and windows

--- src/main.cpp
+++ src/main.cpp
@@ -13,5 +13,7 @@ int main(int argc, char **argv)
 					       G_APPLICATION_FLAGS_NONE);
 
     g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
-    return g_application_run(G_APPLICATION(app), argc, argv);
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
+    g_object_unref (app);
+    return status;
 }
