2b6a78db05fcd80e7e15d914fd88f6e8e83c6911
Takashi Masuyama <mamewotoko@gmail.com>
2021-10-03 13:10:47 +0900

    update ci

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ if [ "$UNAME" = Darwin ]; then
     brew install gtk4
     # make gcc pkg-config
 elif [ -f /etc/lsb-release ]; then
-    # Ubuntu 21.04
+    # Ubuntu 21.04 (not 20.04)
     sudo apt-get update
     sudo apt-get install -y libgtk-4-dev pkg-config \
          make gcc
@@ -19,11 +19,12 @@ elif [ -f /etc/lsb-release ]; then
 
 elif [[ "$UNAME" == "MINGW64_NT"* ]]; then
     # msys2 on PC
-    pacman -Syu --noconfirm
+    # pacman -Syu --noconfirm
     pacman -Sy --noconfirm mingw-w64-x86_64-gtk4 \
            mingw-w64-x86_64-make \
            mingw-w64-x86_64-gcc \
            mingw-w64-x86_64-pkg-config
+
 # elif [[ "$UNAME" == "MSYS_NT"* ]]; then
 #     # travis
 #     $msys2 pacman -Syu --noconfirm
